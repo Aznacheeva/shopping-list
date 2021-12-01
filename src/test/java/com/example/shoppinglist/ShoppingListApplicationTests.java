446bb8d536cd79064294e30702f16a2ba1b7b31d
Sofya <fiepaz03@gmail.com>
2021-12-01 19:22:15 +0500

    fix tests

--- src/test/java/com/example/shoppinglist/ShoppingListApplicationTests.java
+++ src/test/java/com/example/shoppinglist/ShoppingListApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.shoppinglist;
 
-import com.example.shoppinglist.controller.ItemsCreateController;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
